099ae3f1253b02b8e5ecdef6b62c4dd71823fa70
xuejin <xuejin@hctmobile.com>
2018-05-31 20:25:59 +0800

    add interface method

--- src/android/WalletJniPort.java
+++ src/android/WalletJniPort.java
@@ -8,7 +8,26 @@ import org.json.JSONException;
 import android.util.Log;
 
 public class WalletJniPort extends CordovaPlugin{
+    private static final String TAG = "WalletJniPort";
     private static final String TEST = "test";
+    private static final String SETPASSPHRASE = "setPassphrase";
+    private static final String CREATEMNEMONIC = "createMnemonic";
+    private static final String RECOVERYMNEMONIC = "recoveryMnemonic";
+    private static final String DERIVE = "derive";
+    private static final String SIGN = "sign";
+    private static final String DELETEWALLET = "deleteWallet";
+
+    private static final String PASSPHRASE = "passphrase";
+    private static final String LANGUAGE = "language";
+    private static final String NUMBER = "number";
+    private static final String MNEMONIC = "mnemonic";
+    private static final String PATH = "path";
+    private static final String DERIVEALGOID = "deriveAlgoId";
+    private static final String SIGNALGOID = "signAlgoId";
+    private static final String TRANSHASH = "transhash";
+
+    private static final String PUBKEY = "pubkey";
+    private static final String SIGNHASH = "signhash";
     private JSONArray requestArgs;
     private CallbackContext callbackContext;
     /**
@@ -22,23 +41,189 @@ public class WalletJniPort extends CordovaPlugin{
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
         this.requestArgs = args;
-        Log.d("xuejin", "action = " + action);
+        Log.d(TAG, "action = " + action);
         if (action.equals(TEST)) {
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
-                    Log.d("xuejin", "WalletJniPort test start");
+                    Log.d(TAG, "WalletJniPort test start");
                     test();
                 }
             });
-        } else {
+        }else if(action.equals(SETPASSPHRASE)){
+            JSONObject obj = args.optJSONObject(0);
+            if(obj != null){
+                String passphrase = obj.optString(PASSPHRASE);
+                setPassphrase(passphrase);
+            }else {
+                callbackContext.error("User did not specify data to setPassphrase");
+                return true;
+            }
+        }else if(action.equals(CREATEMNEMONIC)){
+            JSONObject obj = args.optJSONObject(0);
+            if(obj != null){
+                String passphrase = obj.optString(PASSPHRASE);
+                String language = obj.optString(LANGUAGE);
+                String number = obj.optString(NUMBER);
+                createMnemonic(passphrase, language, number);
+            }else {
+                callbackContext.error("User did not specify data to createMnemonic");
+                return true;
+            }
+        }else if(action.equals(RECOVERYMNEMONIC)){
+            JSONObject obj = args.optJSONObject(0);
+            if(obj != null){
+                String passphrase = obj.optString(PASSPHRASE);
+                String language = obj.optString(LANGUAGE);
+                String number = obj.optString(NUMBER);
+                String mnemonic = obj.optString(MNEMONIC);
+                recoveryMnemonic(passphrase, language, number, mnemonic);
+            }else {
+                callbackContext.error("User did not specify data to recoveryMnemonic");
+                return true;
+            }
+        }else if(action.equals(DERIVE)){
+            JSONObject obj = args.optJSONObject(0);
+            if(obj != null){
+                String passphrase = obj.optString(PASSPHRASE);
+                String path = obj.optString(PATH);
+                String deriveAlgoId = obj.optString(DERIVEALGOID);
+                derive(passphrase, path, deriveAlgoId);
+            }else {
+                callbackContext.error("User did not specify data to derive");
+                return true;
+            }
+        }else if(action.equals(SIGN)){
+            JSONObject obj = args.optJSONObject(0);
+            if(obj != null){
+                String passphrase = obj.optString(PASSPHRASE);
+                String path = obj.optString(PATH);
+                String deriveAlgoId = obj.optString(DERIVEALGOID);
+                String signAlgoId = obj.optString(SIGNALGOID);
+                String transhash = obj.optString(TRANSHASH);
+                sign(passphrase, path, deriveAlgoId, signAlgoId, transhash);
+            }else {
+                callbackContext.error("User did not specify data to sign");
+                return true;
+            }
+        }else if(action.equals(DELETEWALLET)){
+            JSONObject obj = args.optJSONObject(0);
+            if(obj != null){
+                String passphrase = obj.optString(PASSPHRASE);
+                deleteWallet(passphrase);
+            }else {
+                callbackContext.error("User did not specify data to deleteWallet");
+                return true;
+            }
+        }else {
             return false;
         }
         return true;
     }
 
+    public void setPassphrase(final String passphrase){
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                int ret = set_passphrase(passphrase);
+                if(ret == 1){
+                    WalletJniPort.this.callbackContext.success("ok");
+                }else{
+                    WalletJniPort.this.callbackContext.error("setPassphrase failed");
+                }
+            }
+        });
+    }
 
+    public void createMnemonic(final String passphrase, final String language, final String number){
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                int ret = create_mnemonic(passphrase, language, number);
+                if(ret == 1){
+                    WalletJniPort.this.callbackContext.success("ok");
+                }else{
+                    WalletJniPort.this.callbackContext.error("createMnemonic failed");
+                }
+            }
+        });
+    }
+
+    public void recoveryMnemonic(final String passphrase, final String language, final String number, final String mnemonic){
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                int ret = recovery_mnemonic(passphrase, language, number, mnemonic);
+                if(ret == 1){
+                    WalletJniPort.this.callbackContext.success("ok");
+                }else{
+                    WalletJniPort.this.callbackContext.error("recovery_mnemonic failed");
+                }
+            }
+        });
+    }
+
+    public void derive(final String passphrase, final String path, final String deriveAlgoId){
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                start_derive(passphrase, path, deriveAlgoId, "deriveCallback");
+            }
+        });
+    }
+
+    public void deriveCallback(String pubkey){
+        if(pubkey != null) {
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put(PUBKEY, pubkey);
+            } catch (JSONException e) {
+                Log.d(TAG, "This should never happen");
+            }
+            this.callbackContext.success(obj);
+        }else{
+            this.callbackContext.error("deriveCallback failed");
+        }
+    }
+
+    public void sign(final String passphrase, final String path, final String deriveAlgoId, final String signAlgoId, final String transhash){
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                start_sign(passphrase, path, deriveAlgoId, signAlgoId, transhash, "signCallback");
+            }
+        });
+    }
+
+    public void signCallback(String pubkey, String signhash){
+        if(pubkey != null) {
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put(PUBKEY, pubkey);
+                obj.put(SIGNHASH, signhash);
+            } catch (JSONException e) {
+                Log.d(TAG, "This should never happen");
+            }
+            this.callbackContext.success(obj);
+        }else{
+            this.callbackContext.error("deriveCallback failed");
+        }
+    }
+
+    public void deleteWallet(final String passphrase){
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                int ret = delete_wallet(passphrase);
+                if(ret == 1){
+                    WalletJniPort.this.callbackContext.success("ok");
+                }else{
+                    WalletJniPort.this.callbackContext.error("delete_wallet failed");
+                }
+            }
+        });
+    }
     //JNI
     public native void test();
+    public native int set_passphrase(String passphrase);
+    public native int create_mnemonic(String passphrase, String language, String number);
+    public native int recovery_mnemonic(String passphrase, String language, String number, String mnemonic);
+    public native int start_derive(String passphrase, String path, String deriveAlgoId, String callback);
+    public native int start_sign(String passphrase, String path, String deriveAlgoId, String signAlgoId, String transhash, String callback);
+    public native int delete_wallet(String passphrase);
     static {
         System.loadLibrary("wallet_port");
     }
--- www/WalletJniPort.js
+++ www/WalletJniPort.js
@@ -10,12 +10,154 @@
 
 
     var exec = cordova.require("cordova/exec");
+    var WalletJniPort = function(){};
 
-    var WalletJniPort = function(){
+    //设置密码
+    WalletJniPort.prototype.setPassphrase = function(passphrase, successCallback, errorCallback){
+        console.log("WalletJniPort -- setPassphrase");
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
 
+        if (typeof errorCallback != "function") {
+            console.log("WalletJniPort.setPassphrase failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback != "function") {
+            console.log("WalletJniPort.setPassphrase failure: success callback parameter must be a function");
+            return;
+        }
+        exec(successCallback, errorCallback, "WalletJniPort", "setPassphrase", [passphrase]);
     };
+    //创建助记词
+    WalletJniPort.prototype.createMnemonic = function(passphrase, language, number, successCallback, errorCallback){
+        console.log("WalletJniPort -- createMnemonic");
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
 
-    WalletJniPort.prototype.test = function (content) {
+        if (typeof errorCallback != "function") {
+            console.log("WalletJniPort.createMnemonic failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback != "function") {
+            console.log("WalletJniPort.createMnemonic failure: success callback parameter must be a function");
+            return;
+        }
+        exec(
+            successCallback,
+            errorCallback,
+            "WalletJniPort",
+            "createMnemonic",
+            [{"passphrase": passphrase, "language": language, "number": number}]
+        );
+    };
+    //恢复助记词
+    WalletJniPort.prototype.recoveryMnemonic = function(passphrase, language, number, mnemonic, successCallback, errorCallback){
+        console.log("WalletJniPort -- recoveryMnemonic");
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
+
+        if (typeof errorCallback != "function") {
+            console.log("WalletJniPort.recoveryMnemonic failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback != "function") {
+            console.log("WalletJniPort.recoveryMnemonic failure: success callback parameter must be a function");
+            return;
+        }
+        exec(
+            successCallback,
+            errorCallback,
+            "WalletJniPort",
+            "recoveryMnemonic",
+            [{"passphrase": passphrase, "language": language, "number": number, "mnemonic": mnemonic}]
+        );
+    };
+    //派生
+    WalletJniPort.prototype.derive = function(passphrase, path, deriveAlgoId, successCallback, errorCallback){
+        console.log("WalletJniPort -- derive");
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
+
+        if (typeof errorCallback != "function") {
+            console.log("WalletJniPort.derive failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback != "function") {
+            console.log("WalletJniPort.derive failure: success callback parameter must be a function");
+            return;
+        }
+        exec(
+            successCallback,
+            errorCallback,
+            "WalletJniPort",
+            "derive",
+            [{"passphrase": passphrase, "path": path, "deriveAlgoId": deriveAlgoId}]
+        );
+    };
+    //签名
+    WalletJniPort.prototype.sign = function(passphrase, path, deriveAlgoId, signAlgoId, transhash, successCallback, errorCallback){
+        console.log("WalletJniPort -- sign");
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
+
+        if (typeof errorCallback != "function") {
+            console.log("WalletJniPort.sign failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback != "function") {
+            console.log("WalletJniPort.sign failure: success callback parameter must be a function");
+            return;
+        }
+        exec(
+            successCallback,
+            errorCallback,
+            "WalletJniPort",
+            "sign",
+            [{
+                "passphrase": passphrase,
+                "path": path,
+                "deriveAlgoId": deriveAlgoId,
+                "signAlgoId": signAlgoId,
+                "transhash": transhash
+            }]
+        );
+    };
+    //设置密码
+    WalletJniPort.prototype.deleteWallet = function(passphrase, successCallback, errorCallback){
+        console.log("WalletJniPort -- deleteWallet");
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
+
+        if (typeof errorCallback != "function") {
+            console.log("WalletJniPort.deleteWallet failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback != "function") {
+            console.log("WalletJniPort.deleteWallet failure: success callback parameter must be a function");
+            return;
+        }
+        exec(successCallback, errorCallback, "WalletJniPort", "deleteWallet", [passphrase]);
+    };
+    //测试
+    WalletJniPort.prototype.test = function(content){
         console.log("js test start");
         exec(null, null, "WalletJniPort", "test", [content]);
     };
